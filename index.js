b804e68968daf8d0c6c1657e03904de708fb0e74
elementemerald <22267679+elementemerald@users.noreply.github.com>
2022-05-24 20:14:57 -0600

    Add device info and device list

--- index.js
+++ index.js
@@ -4,5 +4,15 @@ const TuyaClient = require("./lib/tuya");
 const tuya = new TuyaClient(
     process.env.TUYA_CLIENT_ID,
     process.env.TUYA_CLIENT_SECRET,
+    process.env.TUYA_USER_ID,
     "USAW"
 );
+tuya.init().then(() => {
+    tuya.getDeviceInfo("10470056cc50e35eb2bd").then(data => {
+        console.log(data);
+    }).catch(err => {
+        console.error(err);
+    });
+}).catch(err => {
+    console.error(err);
+});
