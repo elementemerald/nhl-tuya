8654d8127e8db7146452b28f63c7c4e41839be75
Ryan Omasta <22267679+elementemerald@users.noreply.github.com>
2022-05-25 17:34:26 -0600

    Modify Tuya API

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 require("dotenv").config();
 
 const TuyaClient = require("./lib/tuya");
+const Game = require("./lib/nhl");
+
 const tuya = new TuyaClient(
     process.env.TUYA_CLIENT_ID,
     process.env.TUYA_CLIENT_SECRET,
     process.env.TUYA_USER_ID,
     "USAW"
 );
+
+tuya.init().then(() => {
+    tuya.getDeviceList().then(data => {
+        const lights = data.result.filter(device => device.category == "dj");
+        for (var i = 0; i < lights.length; i++) {
+            const light = lights[i];
+            console.log(light);
+        }
+    }).catch(err => {
+        console.error(err);
+    });
+}).catch(err => {
+    console.error(err);
+});
