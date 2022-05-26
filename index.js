680123cd98d97a5a0efce69483dda3e1d0ef6f75
elementemerald <22267679+elementemerald@users.noreply.github.com>
2022-05-25 21:59:18 -0600

    Fix API issues

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const TuyaClient = require("./lib/tuya");
 const Game = require("./lib/nhl");
+const teams = require("./teams.json");
 
 const tuya = new TuyaClient(
     process.env.TUYA_CLIENT_ID,
@@ -11,15 +12,20 @@ const tuya = new TuyaClient(
 );
 
 tuya.init().then(() => {
-    tuya.getDeviceList().then(data => {
+    /* tuya.getDeviceList().then(data => {
         const lights = data.result.filter(device => device.category == "dj");
         for (var i = 0; i < lights.length; i++) {
             const light = lights[i];
-            console.log(light);
+            tuya.setDeviceMode(light.id, "colour").then(_ => {
+                setTimeout(tuya.setDeviceMode.bind(tuya), 10000, light.id, "white");
+            }).catch(err => {
+                console.error(err);
+            });
         }
     }).catch(err => {
         console.error(err);
-    });
+    }); */
+    //new Game(tuya, teams.find(team => team.teamCity == "Colorado"));
 }).catch(err => {
     console.error(err);
 });
