1feb8c1b1170197a7d7e4f1d58bdbeb142dec59c
elementemerald <22267679+elementemerald@users.noreply.github.com>
2022-05-26 22:52:04 -0600

    Redundancy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const TuyaClient = require("./lib/tuya");
-const Game = require("./lib/nhl");
+const Game = require("./lib/nhl/Game");
 const teams = require("./teams.json");
 
 const tuya = new TuyaClient(
@@ -11,21 +11,76 @@ const tuya = new TuyaClient(
     "USAW"
 );
 
+const inquirer = require("inquirer");
+
 tuya.init().then(() => {
-    /* tuya.getDeviceList().then(data => {
-        const lights = data.result.filter(device => device.category == "dj");
-        for (var i = 0; i < lights.length; i++) {
-            const light = lights[i];
-            tuya.setDeviceMode(light.id, "colour").then(_ => {
-                setTimeout(tuya.setDeviceMode.bind(tuya), 10000, light.id, "white");
-            }).catch(err => {
-                console.error(err);
-            });
+    inquirer.prompt([
+        /* {
+            type: "confirm",
+            name: "freeGame",
+            message: "Is this a free game? (No team selection)",
+            default: false
+        }, */
+        {
+            type: "list",
+            name: "team",
+            message: "Select Team",
+            choices: teams
         }
-    }).catch(err => {
-        console.error(err);
+    ]).then(answers => {
+        new Game(tuya, answers.team);
+    });
+    /* inquirer.prompt([
+        {
+            type: "list",
+            name: "team",
+            message: "Select Team",
+            choices: teams
+        }
+    ]).then(answers => {
+        const team = answers.team;
+        const [hPrimary, sPrimary, lPrimary] = tuya.toHSL(team.primaryColor);
+        const [hSecondary, sSecondary, lSecondary] = tuya.toHSL(team.secondaryColor);
+
+        tuya.getDeviceList().then(data => {
+            const lights = data.result.filter(device => device.category == "dj");
+            for (var i = 0; i < lights.length; i++) {
+                const light = lights[i];
+                tuya.setDeviceScene(light.id, {
+                    "scene_num": 6,
+                    "scene_units": [
+                        {
+                            "unit_change_mode": "jump",
+                            "unit_switch_duration": 100,
+                            "unit_gradient_duration": 100,
+                            "h": hPrimary,
+                            "s": sPrimary,
+                            "v": 1000,
+                            "bright": 0,
+                            "temperature": 0
+                        },
+                        {
+                            "unit_change_mode": "jump",
+                            "unit_switch_duration": 100,
+                            "unit_gradient_duration": 100,
+                            "h": hSecondary,
+                            "s": sSecondary,
+                            "v": 1000,
+                            "bright": 0,
+                            "temperature": 0
+                        }
+                    ]
+                }).then(_ => {
+                    setTimeout(tuya.setDeviceMode.bind(tuya), 20000, light.id, "white");
+                }).catch(err => {
+                    console.error(err);
+                });
+            }
+        }).catch(err => {
+            console.error(err);
+        });
+        new Game(tuya, answers.team);
     }); */
-    //new Game(tuya, teams.find(team => team.teamCity == "Colorado"));
 }).catch(err => {
     console.error(err);
 });
