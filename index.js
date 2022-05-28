2e435cad23fcb34fa9bd9d1d3470b3cc7e3bb50e
elementemerald <22267679+elementemerald@users.noreply.github.com>
2022-05-28 01:45:09 -0600

    Update teams.json

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ const inquirer = require("inquirer");
 
 tuya.init().then(() => {
     inquirer.prompt([
-        /* {
+        {
             type: "confirm",
             name: "freeGame",
             message: "Is this a free game? (No team selection)",
             default: false
-        }, */
+        },
         {
             type: "list",
             name: "team",
@@ -30,57 +30,6 @@ tuya.init().then(() => {
     ]).then(answers => {
         new Game(tuya, answers.team);
     });
-    /* inquirer.prompt([
-        {
-            type: "list",
-            name: "team",
-            message: "Select Team",
-            choices: teams
-        }
-    ]).then(answers => {
-        const team = answers.team;
-        const [hPrimary, sPrimary, lPrimary] = tuya.toHSL(team.primaryColor);
-        const [hSecondary, sSecondary, lSecondary] = tuya.toHSL(team.secondaryColor);
-
-        tuya.getDeviceList().then(data => {
-            const lights = data.result.filter(device => device.category == "dj");
-            for (var i = 0; i < lights.length; i++) {
-                const light = lights[i];
-                tuya.setDeviceScene(light.id, {
-                    "scene_num": 6,
-                    "scene_units": [
-                        {
-                            "unit_change_mode": "jump",
-                            "unit_switch_duration": 100,
-                            "unit_gradient_duration": 100,
-                            "h": hPrimary,
-                            "s": sPrimary,
-                            "v": 1000,
-                            "bright": 0,
-                            "temperature": 0
-                        },
-                        {
-                            "unit_change_mode": "jump",
-                            "unit_switch_duration": 100,
-                            "unit_gradient_duration": 100,
-                            "h": hSecondary,
-                            "s": sSecondary,
-                            "v": 1000,
-                            "bright": 0,
-                            "temperature": 0
-                        }
-                    ]
-                }).then(_ => {
-                    setTimeout(tuya.setDeviceMode.bind(tuya), 20000, light.id, "white");
-                }).catch(err => {
-                    console.error(err);
-                });
-            }
-        }).catch(err => {
-            console.error(err);
-        });
-        new Game(tuya, answers.team);
-    }); */
 }).catch(err => {
     console.error(err);
 });
